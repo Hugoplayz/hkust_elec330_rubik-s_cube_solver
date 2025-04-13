e6b1db99fc61897df2f7ba3ff8271cb3794a9a94
Grdnoo <gordoncheung87654321@gmail.com>
2025-04-13 20:35:01 +0800

    Update turn.h

--- elec3300/includes/turn.h
+++ elec3300/includes/turn.h
@@ -3,17 +3,17 @@
 
 #include "cube.h"
 
-void left_clockwise(Cube *c);
-void left_counterclockwise(Cube *c);
-void right_clockwise(Cube *c);
-void right_counterclockwise(Cube *c);
-void up_clockwise(Cube *c);
-void up_counterclockwise(Cube *c);
-void down_clockwise(Cube *c);
-void down_counterclockwise(Cube *c);
-void front_clockwise(Cube *c);
-void front_counterclockwise(Cube *c);
-void back_clockwise(Cube *c);
-void back_counterclockwise(Cube *c);
+void left_clockwise(Cube *c);           // L
+void left_counterclockwise(Cube *c);    // L'
+void right_clockwise(Cube *c);          // R
+void right_counterclockwise(Cube *c);   // R'
+void up_clockwise(Cube *c);             // U
+void up_counterclockwise(Cube *c);      // U'
+void down_clockwise(Cube *c);           // D
+void down_counterclockwise(Cube *c);    // D'
+void front_clockwise(Cube *c);          // F
+void front_counterclockwise(Cube *c);   // F'
+void back_clockwise(Cube *c);           // B
+void back_counterclockwise(Cube *c);    // B'
 
 #endif
